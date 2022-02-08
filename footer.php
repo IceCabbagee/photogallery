1dd61390d0081cb6413a9c73808871c98447e144
IceCabbagee <98536952+IceCabbagee@users.noreply.github.com>
2022-02-08 10:05:10 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+// this file contains the closing tags for the body and html tags
+// it is always included at the end of the template file 
+// to close the tags properly
+?>
+<footer>
+<p>&copy;Antonio Fatica </p></footer>
+</body>
+</html>
